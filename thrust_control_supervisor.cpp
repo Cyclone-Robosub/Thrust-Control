cf326e98d1bfdee23e2a9759e5f6de2afe252728
zenyn <zenyn.ethridge@gmail.com>
2025-05-10 18:11:52 -0700

    Restructured everything

---
+++ thrust_control_supervisor.cpp
@@ -0,0 +1,48 @@
+#include "thrust_control_supervisor.hpp"
+
+namespace thrust_control
+{
+
+ThrustControlSupervisor::ThrustControlSupervisor(rclcpp::Logger logger)
+  : logger_(logger)
+{
+  // Constructor body
+}
+
+void ThrustControlSupervisor::step(
+  std::string control_mode,
+  std::array<int, 8> pwm,
+  float duration,
+  std::array<float, 6> position,
+  std::array<float, 6> waypoint)
+{
+	this->control_mode_ = control_mode;
+	this->manual_pwm_ = pwm;
+	this->duration_ = duration;
+	this->current_position_ = position;
+	this->waypoint_ = waypoint;
+
+	this->process_pwm_command(manual_pwm_);
+}
+
+
+void ThrustControlSupervisor::process_pwm_command(std::array<int, 8> pwm)
+{
+  // process_pwm_command implementation
+  if (this->control_mode_ == FEED_FORWARD) { this->feed_forward_pwm(pwm); }
+  if (this->control_mode_ == PID){ this->pid_pwm(pwm); }
+
+
+}  // namespace thrust_control
+
+
+void ThrustControlSupervisor::feed_forward_pwm(std::array<int, 8> pwm)  
+{
+  // feed_forward_pwm implementation
+}
+
+void ThrustControlSupervisor::pid_pwm(std::array<int, 8> pwm)
+{
+  // pid_pwm implementation
+}
+}
---
+++ thrust_control_supervisor.hpp
@@ -0,0 +1,52 @@
+#ifndef THRUST_CONTROL_THRUST_CONTROL_SUPERVISOR_HPP_
+#define THRUST_CONTROL_THRUST_CONTROL_SUPERVISOR_HPP_
+
+#include <string>
+#include <array>
+#include <memory>
+#include <vector>
+#include "rclcpp/rclcpp.hpp"
+
+namespace thrust_control
+{
+
+class ThrustControlSupervisor
+{
+public:
+	
+  explicit ThrustControlSupervisor(rclcpp::Logger logger);
+  void step(	  
+	std::string control_mode,
+       	std::array<int, 8> pwm,
+	float duration,
+	std::array<float, 6> error,
+	std::array<float, 6> waypoint);
+
+private:
+  
+  void set_control_mode(std::string control_mode);
+  void set_manual_pwm(std::array<int, 8> pwm);
+  void set_waypoint(std::array<float, 6> waypoint);
+  void process_pwm_command(std::array<int, 8> pwm_array);
+  void feed_forward_pwm(std::array<int, 8> pwm_array);
+  void pid_pwm(std::array<int, 8> pwm_array);
+ 
+  // Control modes
+  static constexpr const char* FEED_FORWARD = "feed-forward";
+  static constexpr const char* PID = "pid";
+  
+  // Current control state
+  std::string control_mode_;
+  std::array<int, 8> manual_pwm_;
+  std::array<float, 6> current_waypoint_;
+  float duration_;
+  std::array<float, 6> current_position_;
+  std::array<float, 6> waypoint_;
+  
+  // Logging
+  rclcpp::Logger logger_;
+};
+
+}  // namespace thrust_control
+
+#endif  // THRUST_CONTROL_THRUST_CONTROL_SUPERVISOR_HPP_
