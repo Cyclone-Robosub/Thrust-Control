65b542f39bbf8ee1116cdb0e22558309c84b596e
zenyn <zenyn.ethridge@gmail.com>
2025-05-12 02:52:33 -0700

    added command interpreter setup to main.cpp

--- main.cpp
+++ main.cpp
@@ -28,7 +28,13 @@ std::unique_ptr<Command_Interpreter_RPi5> makeCommandInterpreterPtr
       return commandInterpreter_ptr;
     }
 int main(int argc, char * argv[])
-{    
+{
+  std::ostream& output = std::cout;
+  std::ostream& error = std::cerr;
+  std::ostream& log_file = std::cout; //replace with actual log file probably
+
+  auto commandInterpreter_ptr = makeCommandInterpreterPtr(output, error, log_file);
+
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<thrust_control::ThrustControlNode>());
   rclcpp::shutdown();
