776d34dd93cd489d996ca69df7f8104216adf50b
zenyn <zenyn.ethridge@gmail.com>
2025-05-12 01:39:47 -0700

    added funciton to create command interpreter object (stolen from ExecutiveMainLoop)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include "thrust_control_node.hpp"
- 
+#include "Command-Interpreter/lib/Command_Interpreter.h"
+
+
+std::unique_ptr<Command_Interpreter_RPi5> makeCommandInterpreterPtr
+(
+        std::ostream& output, 
+        std::ostream& error, 
+        std::ostream& logFile) 
+{
+    auto PhysicalPins = std::vector<int>{2, 3, 4, 5, 6, 7, 8, 9};
+      std::vector<PwmPin *> thrusterPins;
+      for (auto i : PhysicalPins) 
+      {
+        thrusterPins.push_back(new HardwarePwmPin(i, logFile, output, error));
+      }
+  
+      auto wiringControl = WiringControl(output, logFile, error);
+      std::unique_ptr<Command_Interpreter_RPi5> commandInterpreter_ptr = 
+          std::make_unique<Command_Interpreter_RPi5>(
+          thrusterPins, 
+          std::vector<DigitalPin *>{}, 
+          wiringControl, 
+          logFile, 
+          output, 
+          error);
+  
+      return commandInterpreter_ptr;
+    }
 int main(int argc, char * argv[])
 {    
   rclcpp::init(argc, argv);
