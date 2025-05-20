7eeafed54673e46b46a4e01443ba5d67c42d68dd
zenyn <zenyn.ethridge@gmail.com>
2025-05-19 20:08:09 -0700

    refactoring

--- tests/test_thrust_control_supervisor.cpp
+++ tests/test_thrust_control_supervisor.cpp
@@ -1,26 +1,34 @@
 #include <gtest/gtest.h>
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
-#include "../src/thrust_control_supervisor.hpp"
-#include "../include/Command_Interpreter/src/Command_Interpreter.hpp"
-#include "../src/command_interpreter_pointer.hpp"
+#include "thrust_control_supervisor.hpp"
+#include "Command_Interpreter/src/Command_Interpreter.hpp"
+#include "command_interpreter_pointer.hpp"
 
 class ThrustControlSupervisorTest : public ::testing::Test {
 protected:
-    ThrustControlSupervisorTest() 
-        : logger(rclcpp::get_logger("test_logger")) {
+
+    ThrustControlSupervisorTest() : logger(rclcpp::get_logger("test_logger")) {}
+    
+
+    rclcpp::Node::SharedPtr test_node_sub_;
+    rclcpp::Node::SharedPtr test_node_pub_;
+    std::shared_ptr<rclcpp::executors::SingleThreadedExecutor> executor_;
+
+    void SetUp() override 
+    {
+        if (!rclcpp::ok()) {  rclcpp::init(0, nullptr);}
+        
+
+        // configure test node
+        test_node_sub_ = std::make_shared<rclcpp::Node>("test_subscriber_node");
+        executor_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+        executor_->add_node(test_node_sub_);
     }
 
-    void SetUp() override {
-        if (!rclcpp::ok()) {
-            rclcpp::init(0, nullptr);
-        }
-    }
-
-    void TearDown() override {
-        if (rclcpp::ok()) {
-            rclcpp::shutdown();
-        }
+    void TearDown() override 
+    {
+        if (rclcpp::ok()) { rclcpp::shutdown(); }
     }
 
     rclcpp::Logger logger;
@@ -34,14 +42,24 @@ TEST_F(ThrustControlSupervisorTest, CanBeInitialized) {
     
     // Act - this tests if we can initialize the supervisor without exceptions
     ASSERT_NO_THROW({
-        thrust_control::ThrustControlSupervisor supervisor(logger, std::move(interpreter));
+        thrust_control::ThrustControlSupervisor supervisor(
+                logger, 
+                std::move(interpreter));
     });
 }
 
 TEST_F(ThrustControlSupervisorTest, StepSupervisor) {
 
-    auto interpreter = make_command_interpreter_ptr(std::cout, std::cout, std::cout);
-    thrust_control::ThrustControlSupervisor supervisor(logger, std::move(interpreter));
+    auto interpreter = make_command_interpreter_ptr(
+            std::cout, 
+            std::cout, 
+            std::cout);
+
+    thrust_control::ThrustControlSupervisor supervisor(
+            logger, 
+            std::move(interpreter));
+
+    
 }
 
 
