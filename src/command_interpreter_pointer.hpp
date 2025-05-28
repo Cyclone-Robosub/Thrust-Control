e4ad0cca5bdcd1f161437f0f8c24a6956a34dc98
CRS-AGGIES <crs.aggies@gmail.com>
2025-05-28 16:31:11 -0700

    Fix inconsistent argument order

--- src/command_interpreter_pointer.hpp
+++ src/command_interpreter_pointer.hpp
@@ -5,7 +5,7 @@
 
 // Function to create and return a Command_Interpreter_RPi5 object
 std::unique_ptr<Command_Interpreter_RPi5> make_command_interpreter_ptr(
-    std::ostream& output, 
-    std::ostream& error, 
-    std::ostream& logFile
+    std::ostream& output,
+    std::ostream& logFile,
+    std::ostream& error
 ); 
