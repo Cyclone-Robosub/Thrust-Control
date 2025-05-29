f1f3009c137cc78a7a0bc69a349baf8569ef942c
zenyn <zenyn.ethridge@gmail.com>
2025-05-28 18:44:18 -0700

    added test for pid control with and without error

--- tests/test_thrust_control_supervisor.cpp
+++ tests/test_thrust_control_supervisor.cpp
@@ -4,6 +4,8 @@
 #include "thrust_control_supervisor.hpp"
 #include "Command_Interpreter.hpp"
 #include "command_interpreter_pointer.hpp"
+#include "Command.hpp"
+
 
 class ThrustControlSupervisorTest : public ::testing::Test {
 protected:
@@ -13,6 +15,10 @@ protected:
     void SetUp() override 
     {
         if (!rclcpp::ok()) {  rclcpp::init(0, nullptr);}
+        auto interpreter = make_command_interpreter_ptr(
+                std::cout, 
+                std::cout, 
+                std::cout);
    }
 
     void TearDown() override 
@@ -27,20 +33,54 @@ protected:
 // Test 1: Initialization
 TEST_F(ThrustControlSupervisorTest, CanBeInitialized) {
     // Arrange
-    auto interpreter = make_command_interpreter_ptr(std::cout, std::cout, std::cout);
-    
+         auto test_interpreter = make_command_interpreter_ptr(
+                std::cout, 
+                std::cout, 
+                std::cout);
+
+   
     // Act - this tests if we can initialize the supervisor without exceptions
     ASSERT_NO_THROW({
-        thrust_control::ThrustControlSupervisor supervisor(
+        thrust_control::ThrustControlSupervisor test_supervisor(
                 logger, 
-                std::move(interpreter),
+                std::move(test_interpreter),
                 thrust_control::CommandQueue());
     });
 }
 
 
-TEST_F(ThrustControlSupervisorTest, StepSupervisor) {
+TEST_F(ThrustControlSupervisorTest, StepSupervisorPidNoError) {
 
+    auto interpreter = make_command_interpreter_ptr(
+            std::cout, 
+            std::cout, 
+            std::cout);
+    
+    thrust_control::ThrustControlSupervisor supervisor(
+            logger, 
+            std::move(interpreter),
+            thrust_control::CommandQueue());
+
+    thrust_control::ControlMode control_mode = thrust_control::ControlMode::PID;
+    std::array<float, 6> position = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+    std::array<float, 6> waypoint = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+    
+    ASSERT_NO_THROW({
+        supervisor.step(control_mode, position, waypoint);
+    });
+    
+    pwm_array current_pwm = supervisor.get_current_pwm();
+
+
+    bool has_force = false;
+    for (int i = 0; i < 8; i++) {
+        if (current_pwm.pwm_signals[i] != 1500) {
+            has_force = true;
+        }
+    }
+    EXPECT_FALSE(has_force) << "PWM signal should not have a force";
+}
+TEST_F(ThrustControlSupervisorTest, StepSupervisorPidWithError) {
     auto interpreter = make_command_interpreter_ptr(
             std::cout, 
             std::cout, 
@@ -51,5 +91,21 @@ TEST_F(ThrustControlSupervisorTest, StepSupervisor) {
             std::move(interpreter),
             thrust_control::CommandQueue());
 
+    thrust_control::ControlMode control_mode = thrust_control::ControlMode::PID;
+    std::array<float, 6> position = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+    std::array<float, 6> waypoint = {0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f};
     
-}
+    ASSERT_NO_THROW({
+        supervisor.step(control_mode, position, waypoint);
+    });
+
+    pwm_array current_pwm = supervisor.get_current_pwm();
+
+    bool has_force = false;
+    for (int i = 0; i < 8; i++) {
+        if (current_pwm.pwm_signals[i] != 1500) {
+            has_force = true;
+        }
+    }
+    EXPECT_TRUE(has_force) << "PWM signal should have a force";
+}   
