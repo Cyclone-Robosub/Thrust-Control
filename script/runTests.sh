739d1638fc6dfd405b23d9064557d927af23bfdb
zenyn <zenyn.ethridge@gmail.com>
2025-05-29 18:04:04 -0700

    changed runTest.sh build script so that tests will not run if program does not build

--- script/runTests.sh
+++ script/runTests.sh
@@ -5,16 +5,25 @@
 # Source ROS environment
 source /opt/ros/jazzy/setup.bash
 
- # build crs_common
- cd include/crs_common
- . ./script/build.sh
- cd ../..
+# build crs_common
+cd include/crs_common
+. ./script/build.sh
+if [ $? -ne 0 ]; then
+    echo "ERROR: crs_common build failed"
+    exit 1
+fi
+cd ../..
 
 # Build with testing enabled
 colcon build --cmake-args -DBUILD_TESTING=ON -DMOCK_RPI=ON
+if [ $? -ne 0 ]; then
+    echo "ERROR: Main project build failed"
+    exit 1
+fi
 
 # Source the local setup
 source install/setup.bash
 
-# Run the test executable directly
+# Run the test executable directly (only if builds succeeded)
+echo "Build succeeded, running tests..."
 ./build/thrust_control/thrust_control_tests
