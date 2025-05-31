19ca759ff6c49819f3e6a2f5a5468afbff0f6d6c
zenyn <zenyn.ethridge@gmail.com>
2025-05-30 18:29:28 -0700

    fixed startup script

--- script/startup.sh
+++ script/startup.sh
@@ -1,6 +1,29 @@
-source /opt/ros/jazzy/setup.bash
-colcon build --symlink-install
+#!/bin/bash
 
+# run from /Thrust-Control/
+
+# Source ROS environment
+source /opt/ros/jazzy/setup.bash
+
+# build crs_common
+cd include/crs_common
+. ./script/build.sh
+if [ $? -ne 0 ]; then
+    echo "ERROR: crs_common build failed"
+    exit 1
+fi
+cd ../..
+
+# Build with symlink-install and mock RPI
+colcon build --symlink-install --cmake-args -DMOCK_RPI=ON
+if [ $? -ne 0 ]; then
+    echo "ERROR: Main project build failed"
+    exit 1
+fi
+
+# Source the local setup
 source install/setup.bash
 
+# Run the thrust control node
+echo "Build succeeded, running thrust_control node..."
 ros2 run thrust_control thrust_control
